7924269ef682b52f702b0ae103d6576b5b90e886
Shravani Pande <shravanipande26@gmail.com>
2026-06-25 16:35:25 +0530

    initialized the class scoreManager

--- scoreManager.java
+++ scoreManager.java
@@ -3,3 +3,17 @@ package quizgame;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
+public class scoreManager {
+
+    private static final String HIGH_SCORE_FILE  = "data/highscore.txt";
+    private static final String LEADERBOARD_FILE = "data/leaderboard.txt";
+    private static final int    MAX_LEADERBOARD  = 10;
+
+    private int highScore;
+    private final PriorityQueue<PlayerRecord> leaderboard;
+
+    public scoreManager() {
+        leaderboard = new PriorityQueue<>();
+        loadHighScore();
+        loadLeaderboard();
+    }
